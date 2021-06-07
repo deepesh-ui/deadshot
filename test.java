71ecfd1defda3201461b3f1846cae79caeb278d9
deepesh-ui <deepesh.kumar@harness.io>
2021-06-07 20:10:00 +0530

    feat: added file to be verified

---
+++ test.java
@@ -0,0 +1,17 @@
+package deadshot;
+
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        try {
+            int i = 10;
+            String password = "asdadsa#124112321&aasda";
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+    
+}
